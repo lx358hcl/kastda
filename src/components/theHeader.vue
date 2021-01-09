79a91e8dbe793e8ded2a2518fb6887ccfc33619e
Luka Momcilovic <52632596+lukasinos@users.noreply.github.com>
2021-01-09 19:34:51 +0100

    Ok

--- src/components/theHeader.vue
+++ src/components/theHeader.vue
@@ -1,47 +1,60 @@
 <template>
     <nav class="m-0 p-0 px-3 navbar navbar-expand navbar-light d-flex justify-content-around">
         <div class="container d-flex justify-content-center">
-
             <!-- Innstillinger Modal -->
-            <div class="modal fade" id="innstillingerModal" role="dialog" aria-labelledby="innstillingerModalTittel" aria-hidden="true">
+            <div class="modal fade" id="innstillingerModal" tabindex="-1" role="dialog"
+                aria-labelledby="tidligereKastModalTitle" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered d-flex justify-content-center" role="document">
                     <div class="modal-content">
                         <div class="modal-header m-0 px-2 py-1 text-center border-0">
-                            <div class="d-flex pr-2 pt-2 pb-2 w-100 innerCloseContainer align-items-center justify-content-between">
+                            <div
+                                class="d-flex pr-2 pt-2 pb-2 w-100 innerCloseContainer align-items-center justify-content-between">
                                 <h4 class="m-0 p-0 ml-4 mt-1 closeHeader">Innstillinger</h4>
-                                <button type="button" class="w-auto h-auto m-0 p-0 close d-flex justify-content-center align-items-center" data-dismiss="modal" aria-label="Close">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi x bi-x" viewBox="0 0 16 16">
-                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                <button type="button"
+                                    class="w-auto h-auto m-0 p-0 close d-flex justify-content-center align-items-center"
+                                    data-dismiss="modal" aria-label="Close">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi x bi-x" viewBox="0 0 16 16">
+                                        <path
+                                            d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                                     </svg>
                                 </button>
                             </div>
                         </div>
                         <div class="modal-body text-left ml-0 mb-4">
-                            <ul class="indreModalContainer mt-5 pt-0 text-left">
+                            <ul class="indreModalContainer mt-5 pt-1 text-left">
                                 <li class="list-group-item border-0 mb-4 pb-2">
-                                    <label class="radioContainer">Normalt spill
-                                        <input @click="store.byttInnstilling('normaltspill');" type="radio" data-name="normaltspill" v-bind:checked="store.innstillinger.kastType.normaltspill" name="radio">
-                                        <span class="checkmark"></span>
-                                    </label>
+                                    <a href="#">
+                                        <label class="radioContainer">Normalt spill
+                                            <input @click="store.byttInnstilling('normaltspill');" type="radio"
+                                                data-name="normaltspill"
+                                                v-bind:checked="store.innstillinger.kastType.normaltspill" name="radio">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </a>
                                 </li>
                                 <li class="list-group-item border-0">
-                                    <label class="radioContainer">Ekstra utfordringer
-                                        <input @click="store.byttInnstilling('ekstrautfordringer');"  type="radio" data-name="ekstrautfordringer" v-bind:checked="store.innstillinger.kastType.ekstrautfordringer" name="radio">
-                                        <span class="checkmark"></span>
-                                    </label>
+                                    <a href="#">
+                                        <label class="radioContainer">Ekstra utfordringer
+                                            <input @click="store.byttInnstilling('ekstrautfordringer');" type="radio"
+                                                data-name="ekstrautfordringer"
+                                                v-bind:checked="store.innstillinger.kastType.ekstrautfordringer"
+                                                name="radio">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </a>
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
-
-
             <!-- Navbaren -->
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav d-flex justify-content-around">
                     <li class="nav-item d-flex align-items-center justify-content-center">
-                        <a href="#" data-toggle="modal" data-target="#innstillingerModal" name="innstillinger" v-bind:class="[{ 'aktivNav': false },'nav-link', 'navbar-specs']">
+                        <a href="#" data-toggle="modal" data-target="#innstillingerModal" name="innstillinger"
+                            v-bind:class="[{ 'aktivNav': false },'nav-link', 'navbar-specs']">
                             Innstillinger
                         </a>
                     </li>
@@ -51,7 +64,9 @@
                         </a>
                     </li>
                     <li class="nav-item d-flex align-items-center justify-content-center">
-                        <a @click="store.byttSide('nyRunde')" name="nyRunde" v-bind:class="[{ 'aktivNav': store.erAktiv('nyRunde')},'nav-link', 'navbar-specs']" href="#">
+                        <a @click="store.byttSide('nyRunde')" name="nyRunde"
+                            v-bind:class="[{ 'aktivNav': store.erAktiv('nyRunde')},'nav-link', 'navbar-specs']"
+                            href="#">
                             Ny runde
                         </a>
                     </li>
@@ -62,7 +77,9 @@
 </template>
 
 <script>
-    import { store } from "../store/store.js";
+    import {
+        store
+    } from "../store/store.js";
     export default {
         setup() {
             return {
